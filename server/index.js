ff3b5adc86da448a7d30f8b023dbd5b6f3739de0
chasedurrett <chase.durrett80@gmail.com>
2020-05-19 12:44:00 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -5,12 +5,20 @@ const SERVER_PORT = 4000; // never start a port lower than 3000, and 3000 is res
 
 app.use(express.json()); // parses json to js
 
+app.use(function (req, res, next) {
+  console.log("got em");
+  next()
+});
+
 app.get("/api/chase", (req, res) => {
   res.status(200).send("chase");
 });
 
 app.get("/api/users", ctrl.getAllUsers); // retrieves getAllUsers fn from ctrl (controller)
 app.get("/api/users/:user_id", ctrl.getUserById);
+app.post('/api/users', ctrl.createUser)
+app.put('/api/users/:user_id', ctrl.editUser)
+app.delete('/api/users/:user_id', ctrl.deleteUser)
 
 app.listen(SERVER_PORT, () =>
   console.log(`Server running on port ${SERVER_PORT}`)
